91f4f3fe1f311a0b7600fd311d537da8fb5e9ee6
jeaguilar547-ui <jeaguilar547@gmail.com>
2026-09-19 22:38:31 +0800

    Add project files.

---
+++ AGUILAR-ModernPortfolioMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AGUILAR_ModernPortfolioMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
